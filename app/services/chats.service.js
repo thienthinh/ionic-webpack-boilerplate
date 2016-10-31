0a8c38d9ed6bee64351b47b0df21e41504dc76a7
Thinh Le <thinh.le@asnet.com.vn>
2016-10-31 17:24:58 +0700

    Fix img path

--- app/services/chats.service.js
+++ app/services/chats.service.js
@@ -6,27 +6,27 @@ export default function() {
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'img/ben.png'
+    face: require('img/ben.png')
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    face: require('img/max.png')
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: require('img/adam.jpg')
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: require('img/perry.png')
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: require('img/mike.png')
   }];
 
   return {
